34a84ca5d60053c98a3929c476fd710d284b5c00
Gregory Perry <lotyrin@gmail.com>
2013-07-08 08:44:47 -0700

    Stub step definitions.

---
+++ features/step_definitions/widgetSteps.js
@@ -0,0 +1,73 @@
+var widgetSteps = function() {
+  this.Given(/^there are no widgets$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.When(/^I retrieve the list of widgets$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.Then(/^I should recieve an empty list$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.When(/^I create a widget$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.Then(/^I should see the widget I created$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.Given(/^there is a widget$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.When(/^I retrieve the widget$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.Then(/^I should receive the widget$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.When(/^I update the widget$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.Then(/^I should receive the updated widget$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.When(/^I delete the widget$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.Then(/^I should not see the deleted widget$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.Given(/^there are several widgets$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.Then(/^I should see each of the several widgets$/, function(callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+};
+
+module.exports = widgetSteps;
